087bd90aa4a5cecfc5fc5d6f02913037a50156b7
Hakall <fantasystar_1405@msn.com>
2013-10-25 14:24:27 +0200

    Creation du projet netbeans

---
+++ LBG/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: Alex
+ *
+ * Created on 25 octobre 2013, 13:50
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
