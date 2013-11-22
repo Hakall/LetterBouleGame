806577164ea3dca8ce3bdce28d446740e8f54157
Hakall <fantasystar_1405@msn.com>
2013-11-22 12:17:25 +0100

    Bug

--- LBG/main.c
+++ LBG/main.c
@@ -44,25 +44,6 @@ void algo_1(char* nom_fichier, char* chaine)
     }
 }
  
-/**
- * Affichage fenetre
- */
-void pause()
-{
-    int continuer = 1;
-    SDL_Event event;
-  
-    while (continuer)
-    {
-        SDL_WaitEvent(&event);
-        switch(event.type)
-        {
-            case SDL_QUIT:
-                continuer = 0;
-        }
-    }
-
-}
 
 void bille(SDL_Surface *ecran)
 {   srand(time(NULL));
