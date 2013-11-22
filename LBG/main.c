8a8254bd9e1bb2af74f3c865243e025f41def0c2
jcaballo <julien.caballol@hotmail.fr>
2013-11-22 10:34:53 +0100

    Cercles avec touche bas

--- LBG/main.c
+++ LBG/main.c
@@ -80,17 +80,11 @@ int main(int argc, char** argv) {
     SDL_Init(SDL_INIT_VIDEO);
     ecran=SDL_SetVideoMode(640, 480, 32, SDL_HWSURFACE);
 
-     if (ecran == NULL)
 
     
     //char* fichier_dico = "dico.txt";
     //algo_1(fichier_dico, "asdfarbre");
-    
-    SDL_Surface *ecran = NULL; // Le pointeur qui va stocker la surface de l'écran
-
-    SDL_Init(SDL_INIT_VIDEO);
-    
-    if (SDL_Init(SDL_INIT_VIDEO) == -1)
+    if (ecran == NULL)
     {
         fprintf(stderr, "Erreur d'initialisation de la SDL");
         exit(EXIT_FAILURE);
@@ -114,21 +108,7 @@ int main(int argc, char** argv) {
         }
          
         }
-    
-    ecran = SDL_SetVideoMode(640, 480, 32, SDL_HWSURFACE); // On tente d'ouvrir une fenêtre
-    if (ecran == NULL) // Si l'ouverture a échoué, on le note et on arrête
-    {
-        fprintf(stderr, "Impossible de charger le mode vidéo : %s\n", SDL_GetError());
-        exit(EXIT_FAILURE);
-    }
-    
-    SDL_WM_SetCaption("Letter Boule Game v0.1", NULL);
-    
-    SDL_FillRect(ecran, NULL, SDL_MapRGB(ecran->format, 255, 255, 255));
-    SDL_Flip(ecran);
    
-    
-    pause();
   
     SDL_Quit();
   
